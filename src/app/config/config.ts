b8a670f72d6ed6aebdaedc7af241d2eadecc3ed5
Alvis <aherrera@munily.com>
2025-02-07 23:57:56 -0500

    feat: add environment variables for bot configuration

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,5 +1,9 @@
 export const config = {
   server: {
     port: process.env.PORT ?? 3000,
+    BOT_TOKEN: process.env.BOT_TOKEN,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    CALLBACK_URL: process.env.CALLBACK_URL,
   },
 };
